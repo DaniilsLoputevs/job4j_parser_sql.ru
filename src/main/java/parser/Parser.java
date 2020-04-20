edf0722b1fdf6429b41075082f83c9891707afa8
DaniilsLoputevs <55362320+DaniilsLoputevs@users.noreply.github.com>
2020-04-20 18:56:15 +0100

    temp Parser save

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -2,8 +2,12 @@ package parser;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Parser implements Parse {
 
@@ -17,11 +21,50 @@ public class Parser implements Parse {
 
     @Override
     public List<Post> list(String link) {
-        return null;
+        List<Post> result = null;
+        link = link.substring(0,36);
+        try {
+            Document doc = Jsoup.connect(link).get();
+
+
+            List<String> postsLinks = listOfPostsLinks(doc);
+            var run = true;
+            int pageNum = 1;
+
+
+            while (run) {
+                link = link + pageNum++;
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
     }
 
     @Override
     public Post detail(String link) {
         return null;
     }
+
+//    private List<String> listOfAllLinks(Document doc) {
+//
+//    }
+
+    private List<String> listOfPostsLinks(Document doc) {
+        var tempTable = doc.getElementsByClass("forumTable").first();
+        var tempList = tempTable.getElementsByTag("a").eachAttr("href");
+        return linksFilter(tempList);
+    }
+
+    private List<String> linksFilter(List<String> postsLinks) {
+        return postsLinks.stream()
+                .filter(link -> link.matches(".*[Jj][Aa][Vv][Aa]+.*") &&
+                        !link.matches(".*[Ss][Cc][Rr][Ii][Pp][Tt]+.*"))
+                .collect(Collectors.toList());
+    }
+
+
 }
--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -6,8 +6,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -18,103 +16,157 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
+/*
+TODO - ? Дату публикации вакансии, нужно заносить в базу или нет?
+TODO - Thread.sleep(250 ms) - хороший тон.
+TODO -
+
+*/
 public class ParserTest {
     private String propPath = "./src/main/resources/app.properties";
+    private String connectUlr = "https://www.sql.ru/forum/job-offers/1";
     private Properties config;
     private Connection connection;
     private static final Logger LOG = LogManager.getLogger(ParserTest.class.getName());
 
 //    @Before
-    @Ignore
-    public void setUp() throws Exception {
-        initConfig();
-        initConnection();
-    }
+//    @Ignore
+//    public void setUp() throws Exception {
+//        initConfig();
+//        initConnection();
+//    }
 
     @Test
-    public void testTry() {
+    public void testNewTry() {
         Document doc = null;
+//        String elemList = null;
 //        Element elemList = null;
-        List elemList = null;
+//        Elements elemList = null;
+        List<String> elemList = null;
         Elements juh = null;
         try {
-             doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/1")
-                     .get();
+            doc = Jsoup.connect(connectUlr).get();
 
-            //            String temp = "td#postslisttopic"
-//            String temp = "a#https://www.sql.ru/forum/1324445/senior-java-developer-minsk-3k-4-5";
-//            String temp = "td#postslisttopic.Senior Java developer/ Минск / $3k $4,5";
-
-//            elemList = doc.select("div#tabnews_newsc.content-tabs__items.content-tabs__items_active_true");
-//            elemList = doc.select(temp);
-//            elemList = doc.getElementsByClass("postslisttopic");
-//            elemList = doc.getElementsByTag("a").eachAttr("href");
-//            elemList = doc.getElementsByClass("forumTable");
-
-//            var temp = doc.getElementsByClass("forumTable").first();
-
-//            elemList = temp.getElementsByTag("a");
-//            var temps = temp.getElementsByClass("postslisttopic");
-//            elemList = temp.getElementsByClass("postslisttopic");
-
-//            temp = temp.getElementsByClass("postslisttopic");
-//            elemList = temp.getElementsByClass("postslisttopic");
-
-//            var a = ((Elements) elemList).tagName("td").text();
-//            elemList = List.of(a);
-//            elemList = temps.getElementsByTag("a").textNodes();
-
-//            var t = doc.getElementsByClass("postslisttopic").first();
-//            var t = doc.getElementsByClass("postslisttopic").first();
-//            var g = t.;
+            Element main = doc.getElementsByClass("forumTable").first();
+            elemList = main.getElementsByTag("a").eachAttr("href");
 
 
-//            var temp = doc.getElementsByClass("forumTable");
-//            var temp = doc.getElementsByClass("postslisttopic").eachAttr("a");
-//            temp.
+            var result = elemList;
+            result = result.stream()
+                    .filter(link -> link.matches(".*[Jj][Aa][Vv][Aa]+.*") &&
+                            !link.matches(".*[Ss][Cc][Rr][Ii][Pp][Tt]+.*"))
+                    .collect(Collectors.toList());
 
-//            elemList = temp;
 
-//            Element main = doc.getElementsByClass("forumTable").first();
-            elemList = doc.getElementsByClass("forumTable").tagName("a");
-            elemList = elemList;
+//            doc.
 
-//            List d = main.getElementsByTag("a").textNodes();
-//            elemList = main.getElementsByTag("a").textNodes();
 
-//            temp = temp.getElementsByClass("postslisttopic");
-//            elemList = temp.getElementsByClass("postslisttopic");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-//        if (doc != null) {
-//            for (Element element : elemList.select("a"))
-//                System.out.println(element.text());
-//        } else {
-//            System.out.println("doc = null");
-//        }
-//        if (elemList != null) {
-//            for (Element element : elemList.select("a"))
-//                System.out.println(element.text());
-//        } else {
-//            System.out.println("elemList = null");
-//        }
+//        System.out.println(elemList + " - this link");
 
-
-//        for (var element : elemList)
-//                System.out.println(element);
-
-        for (int i = 0; i < 40; i++) {
-            System.out.println(elemList.get(i));
-        }
-
-//        for (int i = 0; i < elemList.size(); i++) {
+//        for (int i = 0; i < 40; i++) {
 //            System.out.println(elemList.get(i));
 //        }
 
+        System.out.println(connectUlr.substring(0,36));
+        System.out.println(connectUlr + 7);
+
+
+
     }
+
+//    @Test
+//    public void testTry() {
+//        Document doc = null;
+////        Element elemList = null;
+//        List elemList = null;
+//        Elements juh = null;
+//        try {
+//            doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/1")
+//                    .get();
+//
+//            //            String temp = "td#postslisttopic"
+////            String temp = "a#https://www.sql.ru/forum/1324445/senior-java-developer-minsk-3k-4-5";
+////            String temp = "td#postslisttopic.Senior Java developer/ Минск / $3k $4,5";
+//
+////            elemList = doc.select("div#tabnews_newsc.content-tabs__items.content-tabs__items_active_true");
+////            elemList = doc.select(temp);
+////            elemList = doc.getElementsByClass("postslisttopic");
+////            elemList = doc.getElementsByTag("a").eachAttr("href");
+////            elemList = doc.getElementsByClass("forumTable");
+//
+////            var temp = doc.getElementsByClass("forumTable").first();
+//
+////            elemList = temp.getElementsByTag("a");
+////            var temps = temp.getElementsByClass("postslisttopic");
+////            elemList = temp.getElementsByClass("postslisttopic");
+//
+////            temp = temp.getElementsByClass("postslisttopic");
+////            elemList = temp.getElementsByClass("postslisttopic");
+//
+////            var a = ((Elements) elemList).tagName("td").text();
+////            elemList = List.of(a);
+////            elemList = temps.getElementsByTag("a").textNodes();
+//
+////            var t = doc.getElementsByClass("postslisttopic").first();
+////            var t = doc.getElementsByClass("postslisttopic").first();
+////            var g = t.;
+//
+//
+////            var temp = doc.getElementsByClass("forumTable");
+////            var temp = doc.getElementsByClass("postslisttopic").eachAttr("a");
+////            temp.
+//
+////            elemList = temp;
+//
+////            Element main = doc.getElementsByClass("forumTable").first();
+//            elemList = doc.getElementsByClass("forumTable").tagName("a");
+//            elemList = elemList;
+//
+////            List d = main.getElementsByTag("a").textNodes();
+////            elemList = main.getElementsByTag("a").textNodes();
+//
+////            temp = temp.getElementsByClass("postslisttopic");
+////            elemList = temp.getElementsByClass("postslisttopic");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+////        if (doc != null) {
+////            for (Element element : elemList.select("a"))
+////                System.out.println(element.text());
+////        } else {
+////            System.out.println("doc = null");
+////        }
+////        if (elemList != null) {
+////            for (Element element : elemList.select("a"))
+////                System.out.println(element.text());
+////        } else {
+////            System.out.println("elemList = null");
+////        }
+//
+//
+////        for (var element : elemList)
+////                System.out.println(element);
+//
+//        for (int i = 0; i < 40; i++) {
+//            System.out.println(elemList.get(i));
+//        }
+//
+////        for (int i = 0; i < elemList.size(); i++) {
+////            System.out.println(elemList.get(i));
+////        }
+//
+//
+//    }
+
+//    private
 
 
     private void initConfig() {
